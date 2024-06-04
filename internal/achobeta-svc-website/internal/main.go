ccf80874b25ed53b5b349cdb5783cfecfb691481
Jett <jett@moego.pet>
2024-06-04 17:58:29 +0800

    feat(website): add casbin function

--- internal/achobeta-svc-website/internal/main.go
+++ internal/achobeta-svc-website/internal/main.go
@@ -15,15 +15,16 @@ func main() {
 	logFilePath := flag.String("logs", "./logs/", "log file path")
 	casbinPath := flag.String("casbin", "", "specify casbin model path [casbin_model.conf]")
 	flag.Parse()
-	// 初始化配置
+	// 初始化配置, 需要保证config和log先初始化
 	config.InitConfig(*configPath)
 	config.InitLog(*logFilePath)
-	casbin.Init(*casbinPath)
+	// 其他初始化, 顺序自行调整
 	config.InitDatabase()
 	config.InitRedis()
+	// 初始化casbin, 因为要用到数据库的配置，所以要放在数据库初始化之后
+	casbin.Init(*casbinPath)
 	// 初始化服务
 	utils.NewSnowflake()
-	// service.LoadService()
 	/** server 启动要放在最后*/
 	router.RunServer()
 }
