dce22bf19de6ee88cf9753c330f187eb324a21bf
Jett <jetttcc@gmail.com>
2024-06-08 17:30:43 +0800

    feat(authz): move service model

---
+++ internal/achobeta-svc-api/internal/server/service/wire.go
@@ -0,0 +1,28 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.package service
+//
+//go:generate go run -mod=mod github.com/google/wire/cmd/wire
+package service
+
+import (
+	auth_api "achobeta-svc/internal/achobeta-svc-api/internal/api/authz"
+	"achobeta-svc/internal/achobeta-svc-api/internal/logic/auth"
+	"achobeta-svc/internal/achobeta-svc-api/internal/repo/authz"
+
+	"github.com/google/wire"
+)
+
+// 注册的服务都在这里初始化, 更新后执行make wire即可
+// InitServices 初始化所有服务
+func InitServices() *Apis {
+	wire.Build(newApiService,
+		// services
+		auth_api.NewAuthApi,
+		// logic
+		auth.NewLogic,
+		// repo
+		authz.New)
+	return &Apis{}
+}
